f0bc99e1a22b64fc102d58b0ddd0bb89d3c2fcbd
suojae <suojae.developer@gmail.com>
2025-08-29 21:51:17 +0900

    feat: PracticeAddFlutterToIosApp 추가 및 미사용파일 삭제

--- PracticeAddFlutterToIos/App/PracticeAddFlutterToIosApp.swift
+++ PracticeAddFlutterToIos/App/PracticeAddFlutterToIosApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct PracticeAddFlutterToIosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TodoListView(
+                store: Store(initialState: TodoListReducer.State()) {
+                    TodoListReducer()
+                        ._printChanges()
+                }
+            )
         }
     }
 }
