a8d35d93ab334e83ee7bccc389a92c969b38fd1a
suojae <suojae.developer@gmail.com>
2025-08-29 14:46:13 +0900

    Initial Commit

---
+++ PracticeAddFlutterToIos/PracticeAddFlutterToIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAddFlutterToIosApp.swift
+//  PracticeAddFlutterToIos
+//
+//  Created by Jonghyuck Jeon on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAddFlutterToIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
